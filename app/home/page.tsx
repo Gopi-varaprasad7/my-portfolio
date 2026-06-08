90fa7454d45fbbb961a15f6db48181feb3f1f10d
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-06-08 08:01:28 +0530

    Content component is  added

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,18 +6,18 @@ import Experience from '../components/Experience';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
+import ContentCreation from '../components/Content';
 
 export default function Home() {
   return (
-    <div
-      className='bg-black'
-    >
+    <div className='bg-black'>
       <Navbar />
       <Hero />
       <About />
       <Skills />
       <Experience />
       <Projects />
+      <ContentCreation />
       <Contact />
       <Footer />
     </div>
